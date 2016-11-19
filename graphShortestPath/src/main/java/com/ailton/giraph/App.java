4226377bd5a9bf04cc45a61163ea70af108345f8
ailtoncsf <ailtoncsf@yahoo.com.br>
2016-11-19 15:12:24 -0300

    Uso de referência correta do InputFormat.

--- graphShortestPath/src/main/java/com/ailton/giraph/App.java
+++ graphShortestPath/src/main/java/com/ailton/giraph/App.java
@@ -1,22 +1,20 @@
 package com.ailton.giraph;
 
 import org.apache.giraph.GiraphRunner;
-import org.apache.giraph.conf.GiraphClasses;
 import org.apache.giraph.graph.Vertex;
 import org.apache.giraph.io.VertexOutputFormat;
 import org.apache.giraph.job.GiraphJob;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.ToolRunner;
 
 /**
  * Hello world!
  *
  */
-public class App extends GiraphRunner{
-	
+public class App extends GiraphRunner {
+
 	@Override
 	public int run(String[] argArray) throws Exception {
 
@@ -24,30 +22,31 @@ public class App extends GiraphRunner{
 			throw new IllegalArgumentException(
 					"run: Must have 4 arguments <input path> <output path> " + "<source vertex id> <# of workers>");
 		}
-		
+
+		//Job job = new Job(getConf(), getClass().getName());
 		GiraphJob job = new GiraphJob(getConf(), getClass().getName());
-		
-		job.getConfiguration().setVertexClass(Vertex.class);		
+	
+		job.getConfiguration().setVertexClass(Vertex.class);
 		job.getConfiguration().setVertexInputFormatClass(TextTextTextTextInputFormat.class);
 		job.getConfiguration().setVertexOutputFormatClass(VertexOutputFormat.class);
 
-		FileInputFormat.addInputPath(job, new Path(argArray[0]));
-		FileOutputFormat.setOutputPath(job, new Path(argArray[1]));
+		FileInputFormat.addInputPath(job.getInternalJob(), new Path(argArray[0]));
+		FileOutputFormat.setOutputPath(job.getInternalJob(), new Path(argArray[1]));
+		
 		job.getConfiguration().setLong("321.46548748", Long.parseLong(argArray[2]));
-		
-		job.getConfiguration().setWorkerConfiguration(Integer.parseInt(argArray[3]), Integer.parseInt(argArray[3]), 100.0f);		
-		
-		
+
+		//job.getConfiguration().setWorkerConfiguration(Integer.parseInt(argArray[3]), Integer.parseInt(argArray[3]), 100.0f);
+
 		if (job.run(true) == true) {
 			return 0;
 		} else {
 			return -1;
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		System.exit(ToolRunner.run(new App(), args));
-	}	
+	}
 
 }
